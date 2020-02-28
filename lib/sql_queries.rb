3feee2a9976e7d4c5895435e1bf2792b8b499ede
Milan Parmar <milan_parmar@hotmail.co.uk>
2020-02-28 16:14:39 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -52,5 +52,5 @@ def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_b
    INNER JOIN pledges 
    ON projects.id = pledges.project_id
    GROUP BY projects.id
-   ORDER BY projects.title ASC;"
+   WHERE projects.category = 'books';"
 end
